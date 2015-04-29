8661ade51c6872a1146fe45b93f64d6bb6274c39
aceballos <aceballos@6006247a-2d13-4e52-b961-62b44c6c37c8>
2015-04-29 05:41:27 +0000

    Add file headings
    git-svn-id: http://svn.savannah.gnu.org/svn/chess@121 6006247a-2d13-4e52-b961-62b44c6c37c8

--- branches/with_guile/src/script.scm
+++ branches/with_guile/src/script.scm
@@ -1,3 +1,28 @@
+#! GNU Chess 6 - eval.scm: Scheme evaluation
+
+   Copyright (c) 2001-2015 Free Software Foundation, Inc.
+
+   GNU Chess is based on the two research programs
+   Cobalt by Chua Kong-Sian and Gazebo by Stuart Cracraft.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Contact Info:
+     bug-gnu-chess@gnu.org
+     cracraft@ai.mit.edu, cracraft@stanfordalumni.org, cracraft@earthlink.net
+!#
+
 (define do-hello
   (lambda ()
     (display "hello, world---from Scheme")
